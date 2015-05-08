aec615cb71ac874553edb3037b21030c624855a5
Derek Warren <dwarren4096@gmail.com>
2015-05-07 21:57:50 -0700

    Redid game query in addgame Subquery was a good idea. Too bad it didn't work. It's now split into two queries, with the WHERE clause being dynamically generated based on the number of games a user owns

--- addgame.js
+++ addgame.js
@@ -5,24 +5,37 @@ module.exports = {
   add: function(req, res) {
     var UserID = parseInt(req.query.UserID);
     //var GameID = parseInt(req.query.GameID);
-    var qry = mysql.format('SELECT GameID, GameName FROM Games WHERE GameID != (SELECT GROUP_CONCAT(GameID) FROM Game_Library WHERE UserID=?)',
-      UserID);
-    console.log(qry);
-    cxn.connection.query(qry, function(err, result) {
+    var qry1 = mysql.format('SELECT GameID FROM Game_Library WHERE UserID=?', UserID);
+    console.log(qry1);
+    cxn.connection.query(qry1, function(err, result) {
       if (err) {cxn.handleError(res, err);}
       else {
-        var responseHTML = cxn.HTMLHeader + '<h1>Adding a game to UserID '+UserID+'\'s library</h1>\n\
-          <h2>Select a game</h2>\n\
-          <form action="/addgame/submit" method="post">\n\
-          <input type="hidden" name="UserID" value="'+UserID+'" />\n\
-          <select name="GameID">\n';
-        for (var i=0; i<result.length; i++) {
-          responseHTML+='<option value="'+result[i].GameID+'">'+result[i].GameName+'</option>\n';
+        var q1Result = result;
+        var qry2 = 'SELECT GameID, GameName FROM Games';
+        if (q1Result.length<=1) {
+          qry2 += ' WHERE GameID != ' + q1Result[0].GameID;
+          for (var i=1; i<q1Result.length; i++) {
+            qry2 += ' AND GameID != ' + q1Result[i].GameID;
+          }
         }
-        responseHTML+='</select>\n\
-          <input type="submit" value="Submit" />\n\
-          </form>\n' + cxn.HTMLFooter;
-        res.send(responseHTML);
+        console.log(qry2);
+        cxn.connection.query(qry2, function(err, result) {
+          if (err) {cxn.handleError(res, err);}
+          else {
+            var responseHTML = cxn.HTMLHeader + '<h1>Adding a game to UserID '+UserID+'\'s library</h1>\n\
+              <h2>Select a game</h2>\n\
+              <form action="/addgame/submit" method="post">\n\
+              <input type="hidden" name="UserID" value="'+UserID+'" />\n\
+              <select name="GameID">\n';
+            for (var i=0; i<result.length; i++) {
+              responseHTML+='<option value="'+result[i].GameID+'">'+result[i].GameName+'</option>\n';
+            }
+            responseHTML+='</select>\n\
+              <input type="submit" value="Submit" />\n\
+              </form>\n' + cxn.HTMLFooter;
+            res.send(responseHTML);
+          }
+        });
       }
     });
   },
